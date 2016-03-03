839d288301a6c29d3380e24db92432579dd29329
Josh Silverstein <silverjosh90@gmail.com>
2016-03-03 15:49:09 -0700

    test route

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/name', function(req, res, next) {
+  console.log('Worked~!!!');
+  console.log(req.body)
+  res.send('good job')
 });
+
+
+
+
+
+
+
 
 module.exports = router;

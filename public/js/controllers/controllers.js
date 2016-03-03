08786019d1d38edfcce13ffb8846f4f4acbf3ea7
Josh Silverstein <silverjosh90@gmail.com>
2016-03-03 16:27:06 -0700

    post working

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -1,5 +1,29 @@
-app.controller('splashController', ['$scope', function($scope, $routeParams) {
+app.controller('splashController', ['$scope', '$http', '$location', function($scope, $http, $location) {
   $scope.lolz = "Hello Wizarding World!!!"
+  var request = $http({
+  method: 'POST',
+  url: 'http://localhost:3000/api/name',
+  data:
+  {
+    flavor: 'tacos',
+    description: 'boss',
+    imageUrl: 'daSheet'
+  },
+  headers: {
+      'permission': '/*/',
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods' : 'POST, GET, PUT, DELETE',
+    }
+})
+  request.success(function(results) {
+    console.log(results)
+    $scope.lolz = results
+  })
+// $http.get('http://localhost:3000/api/name').then(function(results){
+//   console.log(results.data);
+// })
+
+
 }])
 
 app.controller('surveyController', ['$scope', function($scope, $routeParams) {
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/name', function(req, res, next) {
+router.post('/name', function(req, res, next) {
   console.log('Worked~!!!');
   console.log(req.body)
   res.send('good job')

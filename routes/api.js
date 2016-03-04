dee65c7a665632ce60165f26d7a0a13829495ab3
Josh Silverstein <silverjosh90@gmail.com>
2016-03-03 17:13:18 -0700

    Access Secret Working

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').load();
 
 /* GET users listing. */
 router.post('/name', function(req, res, next) {
   console.log('Worked~!!!');
   console.log(req.body)
-  res.send('good job')
+  req.body.token = process.env.access_secret
+  
 });
 
 

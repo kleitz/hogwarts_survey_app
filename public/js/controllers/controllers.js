b988967eaa2b3d8263fd2e360fe4a092661832cd
Mikel <mikelbrierly@gmail.com>
2016-03-06 08:07:38 -0700

    survey function almost working, user answers and dominant house are printing, but still need to send to API

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -38,14 +38,74 @@ apicalls.getAll().then(function(results){
 
 }])
 
+
+//This is our controller for our survey
 app.controller('surveyController', ['$scope', 'survey', '$location', '$auth', function($scope, survey, $location, $auth) {
+  arr = [];
+  $scope.getSorted = function() {
+    for (var key in $scope.sortingHat) {
+      if ($scope.sortingHat.hasOwnProperty(key)) {
+        // console.log($scope.sortingHat[key]);
+        arr.push($scope.sortingHat[key])
+      }
+      var questionIndex = arr.indexOf(Math.max(...arr));
+    }
+    arr.pop()
+    if (questionIndex === 0) {
+      $scope.sortingHat.dominantHouse = 'gryffindor'
+    }
+    if (questionIndex === 1) {
+      $scope.sortingHat.dominantHouse = 'hufflepuff'
+    }
+    if (questionIndex === 2) {
+      $scope.sortingHat.dominantHouse = 'slytherin'
+    }
+    if (questionIndex === 3) {
+      $scope.sortingHat.dominantHouse = 'ravenclaw'
+    }
+    console.log($scope.sortingHat);
+  }
+
+  $scope.answerSelect = function(answer) {
+    if (answer.house === 'gryffindor') {
+      $scope.sortingHat.gryffindor += 1;
+    }
+    if (answer.house === 'slytherin') {
+      $scope.sortingHat.slytherin += 1;
+    }
+    if (answer.house === 'ravenclaw') {
+      $scope.sortingHat.ravenclaw += 1;
+    }
+    if (answer.house === 'hufflepuff') {
+      $scope.sortingHat.hufflepuff += 1;
+    }
+    console.log($scope.sortingHat);
+  }
+
+  $scope.sortingHat = {};
+  $scope.sortingHat.gryffindor = 0;
+  $scope.sortingHat.hufflepuff = 0;
+  $scope.sortingHat.slytherin = 0;
+  $scope.sortingHat.ravenclaw = 0;
+  $scope.sortingHat.dominantHouse = '';
   $scope.survey = survey.surveyQuestions;
-  $scope.whatAnswer = function(item) {
-    console.log(item);
-  }
-  $scope.houseSurvey= function(question) {
-    console.log(question.answers);
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   $scope.authenticate = function(provider) {
     $auth.authenticate(provider).then(function(results){
